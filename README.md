c71cb3b8931ac2c4aceb0d5fab0fc06d70ef9cfa
TheNerdyCat <edwardsims30@live.com>
2019-10-15 18:26:48 +0100

    added requirements file and completed scraping script

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # sca-violations-automated-scrape
-In this script we send a request to a website to check for any updates to their SCA violations table. If there has been an update (besides a previous violation being removed), we will send an email notification as a reminder.
 
+## Introduction
+In this script we send a request to a website to check for any updates to their SCA violations table. If there has been an update (besides a previous violation being removed), we will send an email notification as a reminder.
 The idea behind this was to avoid having to manually check for violations, remembering to do so yourself. With this script, we can add it to Windows scheduler as a script that is run once a day automatically. It's the little things that make the difference!
 
-An email account has to be created for this script to work, and your own address and password needs to be supplied. I have hidden mine for obvious reasons.
+## Issues
+The table is dynamically loaded with javascript, which means that the content is loaded *after* the HTML. This is tricky because a normal request using `requests` and `BeautifulSoup` will not obtain the content we want, as it scrapes too early.
+Instead we have to use a webdriver with Selenium. Given that we wanted a hassle free solution this is not ideal, but if any readers finds a better solution I am eager to listen!
 
+## Tips
+An email account has to be created for this script to work, and your own address and password needs to be supplied. I have hidden mine for obvious reasons.
 I would recommend for solutions like this, to create a new email for these purposes. **I DO NOT CONDONE USING YOUR OWN PERSONAL EMAIL ADDRESS AS THIS CAN BE SUBJECT TO SECURITY RISKS!**
 
+
 Website can be found [here](https://www.sca.gov.ae/en/open-data/publishing-names-of-violators/violations-committed-by-investors.aspx?page=1#page=1).
+
+## Requirements
+ - Can be installed using anaconda prompt and the requirements.txt file. Type `conda install --file requirements.txt`
+ - Packages that are used but should be included in Python alread are ssl, smtplib, time.
---
+++ sca_violations_automated_scrape.py
@@ -0,0 +1,56 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import ctypes  # For popup messages - included
+import smtplib, ssl  # For email server - included
+
+# Read previous table
+try:
+    table_content_previous = open("Output.txt", "r").read()
+    table_content_previous.close()
+except:
+    table_content_previous = open("Output.txt", "w")
+    table_content_previous.write('')  # Create blank .txt file if there isn't already one
+    table_content_previous.close()
+page_link = 'https://www.sca.gov.ae/en/open-data/publishing-names-of-violators/violations-committed-by-investors.aspx?page=1#page=1'
+# Start web driver - error message if update is needed
+try:
+    browser = webdriver.Chrome('C:/Users/Edward Sims/Downloads/chromedriver.exe')
+except:
+    ctypes.windll.user32.MessageBoxW(0, "Please update web driver.", "Web Driver Error", 1)
+
+# Find page - message if error 4o4
+try:
+    browser.get(page_link)
+except:
+    ctypes.windll.user32.MessageBoxW(0, "Page not found. Check link is correct.", "Error 404", 1)
+# Locate table HTML
+table_dest = browser.find_element_by_class_name('general-listing')
+table_content_current = str(BeautifulSoup(table_dest.get_attribute('innerHTML')))
+browser.close()
+# Create the email content for the notification
+email_content = """\
+Subject: SCA Violations Update
+​
+​
+Click below to view the update:
+https://www.sca.gov.ae/en/open-data/publishing-names-of-violators/violations-committed-by-investors.aspx?page=1#page=1"""
+# Compare current table to previous. Send notification if different.
+if table_content_current != table_content_previous:
+    try:
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        sender_email = "noreply.thenerdycat@gmail.com"  # Enter your address
+        receiver_email = "noreply.thenerdycat@gmail.com"  # Enter receiver address
+        password = ''
+        message = email_content
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+    except:
+        ctypes.windll.user32.MessageBoxW(0, "Please check email details are correct.", "Email Server Error", 1)
+# Save updated table to file
+table_content_updated = open("Output.txt", "w")
+table_content_updated.write(table_content_current)
+table_content_updated.close()
